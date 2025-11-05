973b4971a430611f545d7ca3ec1408b4d909b35c
Aship-6729 <aalamaship98@gmail.com>
2025-11-05 11:56:23 +0545

    Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class Circle
+{
+    const double PI = 3.14;
+
+    public static void Main(String[] args)
+    {
+        PI = 3.123
+    }
+}
