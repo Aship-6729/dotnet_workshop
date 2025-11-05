a40ba00fad2197961b2f1900f946b2617341d6f5
Aship-6729 <aalamaship98@gmail.com>
2025-11-05 12:15:06 +0545

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] num = { 1, 4, 2,6, 5 };
+
+Array.Sort(num);
+Array.Reverse(num);
+
+for(int i=0; i< num.Length; i++)
+{
+    Console.WriteLine(num[i]);
+}
+
+Array.IndexOf(num, 4);
