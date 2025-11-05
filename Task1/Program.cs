896accf47ebf0950125af6ceb7d42cc6a5f83746
Aship-6729 <aalamaship98@gmail.com>
2025-11-05 12:23:32 +0545

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+string userName;
+userName = 'Mohammad Aship';
+
+int luckyNumber;
+luckyNumber = 18;
+
+Console.WriteLine($"Hello, {userName}. Your lucky number is {luckyNumber}.");
